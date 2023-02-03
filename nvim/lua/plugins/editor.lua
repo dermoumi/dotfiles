bd08a8a80de905e1ad52ba0d3e27fa91a2f119ff
S Dermoumi <hi@sdrm.me>
2023-02-03 08:20:52 +0100

    Minor formatting

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -44,67 +44,78 @@ return {
           vim.schedule(function() gs.next_hunk() end)
           return "<Ignore>"
         end, {
-            desc = "Next hunk",
-            buffer = buf_nr,
-          })
+          desc = "Next hunk",
+          buffer = buf_nr,
+        })
 
         vim.keymap.set("n", "[c", function()
           if vim.wo.diff then return "[c" end
           vim.schedule(function() gs.prev_hunk() end)
           return "<Ignore>"
         end, {
-            desc = "Prev hunk",
-            buffer = buf_nr,
-          })
+          desc = "Prev hunk",
+          buffer = buf_nr,
+        })
 
         vim.keymap.set("n", "<leader>hp", gs.preview_hunk, {
           desc = "Previw hunk",
           buffer = buf_nr,
         })
-        vim.keymap.set("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", {
+
+        vim.keymap.set("n", "<leader>hs", gs.stage_hunk, {
           desc = "Stage hunk",
           buffer = buf_nr,
         })
+
         vim.keymap.set("n", "<leader>hu", gs.undo_stage_hunk, {
           desc = "Undo hunk",
           buffer = buf_nr,
         })
-        vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", {
+
+        vim.keymap.set("n", "<leader>hr", gs.reset_hunk, {
           desc = "Revert hunk",
           buffer = buf_nr,
         })
+
         vim.keymap.set("n", "<leader>hS", gs.stage_buffer, {
           desc = "Stage buffer",
           buffer = buf_nr,
         })
+
         vim.keymap.set("n", "<leader>hU", gs.reset_buffer, {
           desc = "Reset buffer",
           buffer = buf_nr,
         })
+
         vim.keymap.set("n", "<leader>hR", gs.reset_buffer_index, {
           desc = "Revert buffer",
           buffer = buf_nr,
         })
+
         vim.keymap.set("n", "<leader>hb", function()
           gs.blame_line({ full = true })
         end, {
-            desc = "Blame line",
-            buffer = buf_nr,
-          })
+          desc = "Blame line",
+          buffer = buf_nr,
+        })
+
         vim.keymap.set("n", "<leader>hB", gs.toggle_current_line_blame, {
           desc = "Toggle current line blame",
           buffer = buf_nr,
         })
+
         vim.keymap.set("n", "<leader>hd", gs.diffthis, {
           desc = "Show diff",
           buffer = buf_nr,
         })
+
         vim.keymap.set("n", "<leader>hD", function()
           gs.diffthis("~")
         end, {
-            desc = "Show diff to HEAD",
-            buffer = buf_nr,
-          })
+          desc = "Show diff to HEAD",
+          buffer = buf_nr,
+        })
+
         vim.keymap.set("n", "<leader>hH", gs.toggle_deleted, {
           desc = "Toggle deleted",
           buffer = buf_nr,
