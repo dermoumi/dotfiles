564a44f8d8d49be78295be2ceabc5f783b75f106
S Dermoumi <hi@sdrm.me>
2025-01-20 10:00:47 +0100

    Hide some notifications in neovim

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -9,5 +9,27 @@ return {
     presets = {
       command_palette = true,
     },
+    routes = {
+      {
+        -- Duplicate notifaction for when quitting without saving
+        filter = { find = "E162" },
+        opts = { skip = true },
+      },
+      {
+        -- Messages when a buffer is written
+        filter = { find = ".+B written" },
+        opts = { skip = true },
+      },
+      {
+        -- Message when a file is opened
+        filter = { find = "^\".+\" %d+L, %d+B$" },
+        opts = { skip = true },
+      },
+      {
+        -- Message when a new file is opened
+        filter = { find = "^\".+\" %[New%]$" },
+        opts = { skip = true },
+      },
+    },
   },
 }
