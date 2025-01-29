7c2579a76b159f4bcee8c51942d8a31c8e7392ad
S Dermoumi <hi@sdrm.me>
2025-01-29 19:32:31 +0100

    Update setup script, currently for osx only

--- README.md
+++ README.md
@@ -1,73 +1,8 @@
 ## Install
 
-Linux instructions
+Macos instructions:
 
 ```bash
-# Upgrade and install packages
-sudo apt update -yq && sudo apt upgrade -yq
-sudo apt install -yq zsh unzip git curl tmux
-
-# Download repo (assuming ssh keys are already setup)
-git clone --recurse-submodules git@github.com:dermoumi/dotfiles.git ~/.dotfiles
-
-# Setup zsh and link dotfiles
-ZDOTDIR=~/.dotfiles/zsh zsh -ic 'dotfiles link'
-
-# Set default shell to zsh
-chsh -s $(which zsh)
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh -s -- -a -c
 ```
 
-## Next steps
-
-### Install build essentials
-
-```bash
-sudo apt install build-essential
-```
-
-### Install neovim
-
-```bash
-install_nvim
-```
-
-### Install pyenv and python
-
-```bash
-# Install Python build dependencies (https://github.com/pyenv/pyenv/wiki)
-# For ubuntu this would be:
-sudo apt-get install make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-curl https://pyenv.run | bash
-pyenv install 3.10.4
-pyenv global 3.10.4
-```
-
-### Install neovim remote
-
-```bash
-pip install neovim-remote
-```
-
-### Install sdkman
-
-```bash
-# For android sdk 30 development
-
-curl -s "https://get.sdkman.io" | bash
-sdk install java 8.0.332-zulu
-sdk install gradle
-
-export ANDROID_SDK_ROOT=$HOME/.android/sdk
-mkdir -p "$ANDROID_SDK_ROOT"
-sudo apt-get install sdkmanager # Ubuntu 22.04+
-sudo sdkmanager --sdk_root=~/.android/sdk --install 'platforms;android-30' 'build-tools;30.0.3' platform-tools tools
-```
-
-### Install rustup
-
-```bash
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-```
--- setup.sh
+++ setup.sh
@@ -11,14 +11,10 @@ if ((is_macos)); then
     app_check_list+=("brew")
 fi
 
-app_install_list=()
-app_update_list=()
-
 force=0
-update_apps=0
-
-installed=0
-failed=0
+clone=0
+install_desktop_apps=0
+install_utilities=0
 
 # Parse arguments
 while :; do
@@ -27,8 +23,21 @@ while :; do
             force=1
             shift
             ;;
-        -u|--update)
-            update_apps=1
+        -c|--clone)
+            clone=1
+            shift
+            ;;
+        --desktop)
+            install_desktop_apps=1
+            shift
+            ;;
+        --utilities)
+            install_utilities=1
+            shift
+            ;;
+        -a|--all)
+            install_desktop_apps=1
+            install_utilities=1
             shift
             ;;
         *)
@@ -36,8 +45,24 @@ while :; do
     esac
 done
 
+# Clone the repo
+__clone_repo() {
+    if ! ((clone)); then
+        return
+    fi
+
+    echo "Cloning the repo..."
+    repo_https_url="https://github.com/dermoumi/dotfiles.git"
+    repo_ssh_url="git@github.com:dermoumi/dotfiles.git"
+    dotfiles_dir="$HOME/.dotfiles"
+
+    git clone "$repo_https_url" "$dotfiles_dir"
+    cd "$dotfiles_dir"
+    git remote set-url origin "$repo_ssh_url"
+}
+
 # Utility to link source to target
-mk_link() {
+__mk_link() {
     local source=$1
     local target_dir=$2
 
@@ -54,347 +79,123 @@ mk_link() {
     ln -Ffs "$PWD/$source" "$target_dir"
 }
 
+__make_links() {
+    echo "Linking files..."
+    __mk_link zsh/.zshenv ~/
+    __mk_link .gitconfig ~/
+    __mk_link .fdignore ~/
+    __mk_link .tmux.conf ~/
+    __mk_link wezterm ~/.config/
+    __mk_link nvim ~/.config/
+
+    # MacOS specific links
+    if ((is_macos)); then
+        __mk_link karabiner ~/.config/
+    fi
+}
+
 # Checks if the given app is installed
-check_app_installed() {
+__check_app_installed() {
     local app=$1
     command -v $app &>/dev/null
 }
 
-# Link files
-echo "Linking files..."
-mk_link zsh/.zshenv ~/
-mk_link .gitconfig ~/
-mk_link .fdignore ~/
-mk_link .tmux.conf ~/
-mk_link wezterm ~/.config/
-mk_link nvim ~/.config/
-
-# MacOS specific links
-if ((is_macos)); then
-    mk_link karabiner ~/.config/
-fi
-
-# Check for apps
-echo "Checking for required apps..."
-for app in "${app_check_list[@]}"; do
-    if ! check_app_installed $app; then
-        app_install_list+=($app)
-    elif ((update_apps)); then
-        app_update_list+=($app)
+__ensure_homebrew_installed() {
+    if __check_app_installed brew; then
+        return
     fi
-done
-echo "Apps to install: ${app_install_list[@]}"
-echo "Apps to update: ${app_update_list[@]}"
 
-__latest_url() {
-    local repo filename_pattern
-    repo=$1
-    filename_pattern=$2
-
-    curl -fsSL "https://api.github.com/repos/$repo/releases/latest" | grep -oE "https://.+/$filename_pattern" | head -n1
-}
-
-__try_curl() {
-    command -v curl > /dev/null || return 1
-
-    local file=$1
-    shift
-
-    if [[ "$file" =~ tar.gz$ ]]; then
-        curl -fsSL "$file" | tar -xzf - "$@"
-    else
-        local tempdir
-        tempdir=$(mktemp -d)
-        local temp="$tempdir/target.zip"
-        if [[ "$tempdir" ]]; then
-            curl -fsSLo "$temp" "$file" && unzip -o "$temp" >/dev/null
-            rm -rf "$tempdir"
-        fi
-    fi
-}
-
-__try_wget() {
-    command -v wget > /dev/null || return 1
-
-    local file=$1
-    shift
-
-    if [[ "$file" =~ tar.gz$ ]]; then
-        wget -O - "$file" | tar -xzf - "$@"
-    else
-        local tempdir
-        tempdir=$(mktemp -d)
-        local temp="$tempdir/target.zip"
-        if [[ "$tempdir" ]]; then
-            wget -O "$temp" "$file" && unzip -o "$temp" >/dev/null
-            rm -rf "$tempdir"
-        fi
-    fi
-}
-
-__download() {
-    local retval=0
-
-    echo "-> Downloading from: $1"
-
-    set -o pipefail
-    __try_curl "$@" || __try_wget "$@" || retval=$?
-    set +o pipefail
-
-    return "$retval"
-}
-
-# Install apps
-install_fzf() {
-    echo "Installing fzf..."
-
-    if command -v brew &>/dev/null; then
-        brew install fzf
-    else
-        # Try to download binary executable
-        archi=$(uname -sm)
-        case "$archi" in
-            Linux\ aarch64*) file_pattern="fzf-[^-]+-linux_arm64\.tar\.gz"   ;;
-            Linux\ *64)      file_pattern="fzf-[^-]+-linux_amd64\.tar\.gz"   ;;
-            CYGWIN*\ *64)    file_pattern="fzf-[^-]+-windows_amd64\.zip"     ;;
-            MINGW*\ *64)     file_pattern="fzf-[^-]+-windows_amd64\.zip"     ;;
-            MSYS*\ *64)      file_pattern="fzf-[^-]+-windows_amd64\.zip"     ;;
-            Windows*\ *64)   file_pattern="fzf-[^-]+-windows_amd64\.zip"     ;;
-            *)               file_pattern= binary_error=1 ;;
-        esac
-
-        __download "$(__latest_url "junegunn/fzf" "$file_pattern")"
-
-        if [ -f fzf ]; then
-            chmod +x fzf
-            installed=1
-        else
-            echo "Failed to download fzf" >&2
-            failed=1
-        fi
-    fi
-}
-
-install_fd() {
-    echo "Installing fd..."
-
-    if command -v brew &>/dev/null; then
-        brew install fd
-    else
-        # Try to download binary executable
-        archi=$(uname -sm)
-        case "$archi" in
-            Linux\ aarch64*) file_pattern="fd-[^-]+-arm-unknown-linux-musleabihf\.tar\.gz"   ;;
-            Linux\ *64)      file_pattern="fd-[^-]+-x86_64-unknown-linux-musl\.tar\.gz"   ;;
-            CYGWIN*\ *64)    file_pattern="fd-[^-]+-i686-pc-windows-gnu\.zip"     ;;
-            MINGW*\ *64)     file_pattern="fd-[^-]+-i686-pc-windows-gnu\.zip"     ;;
-            MSYS*\ *64)      file_pattern="fd-[^-]+-i686-pc-windows-gnu\.zip"     ;;
-            Windows*\ *64)   file_pattern="fd-[^-]+-i686-pc-windows-gnu\.zip"     ;;
-            *)               file_pattern= binary_error=1 ;;
-        esac
-
-        __download "$(__latest_url "sharkdp/fd" "$file_pattern")" \
-            --wildcards '*/fd' --strip-components=1
-
-        if [ -f fd ]; then
-            chmod +x fd
-            installed=1
-        else
-            echo "Failed to download fd" >&2
-            failed=1
-        fi
-    fi
-}
-
-install_zoxide() {
-    echo "Installing zoxide..."
-
-    if command -v brew &>/dev/null; then
-        brew install zoxide
-    else
-        # Try to download binary executable
-        archi=$(uname -sm)
-        case "$archi" in
-            Linux\ aarch64*) file_pattern="zoxide(-.+)?-aarch64-unknown-linux-musl\.tar\.gz"   ;;
-            Linux\ *64)      file_pattern="zoxide(-.+)?-x86_64-unknown-linux-musl\.tar\.gz"   ;;
-            CYGWIN*\ *64)    file_pattern="zoxide(-.+)?-x86_64-pc-windows-msvc\.zip"     ;;
-            MINGW*\ *64)     file_pattern="zoxide(-.+)?-x86_64-pc-windows-msvc\.zip"     ;;
-            MSYS*\ *64)      file_pattern="zoxide(-.+)?-x86_64-pc-windows-msvc\.zip"     ;;
-            Windows*\ *64)   file_pattern="zoxide(-.+)?-x86_64-pc-windows-msvc\.zip"     ;;
-            *)               file_pattern= binary_error=1 ;;
-        esac
-
-        __download "$(__latest_url "ajeetdsouza/zoxide" "$file_pattern")" --wildcards zoxide
-
-        if [ -f zoxide ]; then
-            chmod +x zoxide
-            installed=1
-        else
-            echo "Failed to download zoxide" >&2
-            failed=1
-        fi
-    fi
-}
-
-install_bat() {
-    echo "Installing bat..."
-
-    if command -v brew &>/dev/null; then
-        brew install bat
-    else
-        # Try to download binary executable
-        archi=$(uname -sm)
-        case "$archi" in
-            Linux\ aarch64*) file_pattern="bat-[^-]+-aarch64-unknown-linux-gnu\.tar\.gz"   ;;
-            Linux\ *64)      file_pattern="bat-[^-]+-x86_64-unknown-linux-musl\.tar\.gz"   ;;
-            CYGWIN*\ *64)    file_pattern="bat-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            MINGW*\ *64)     file_pattern="bat-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            MSYS*\ *64)      file_pattern="bat-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            Windows*\ *64)   file_pattern="bat-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            *)               file_pattern= binary_error=1 ;;
-        esac
-
-        __download "$(__latest_url "sharkdp/bat" "$file_pattern")" \
-            --wildcards '*/bat' --strip-components=1
-
-        if [ -f bat ]; then
-            chmod +x bat
-            installed=1
-        else
-            echo "Failed to download bat" >&2
-            failed=1
-        fi
-    fi
-}
-
-install_rg() {
-    echo "Installing ripgrep..."
-
-    if command -v brew &>/dev/null; then
-        brew install ripgrep
-    else
-        # Try to download binary executable
-        archi=$(uname -sm)
-        case "$archi" in
-            Linux\ aarch64*) file_pattern="ripgrep-[^-]+-aarch64-unknown-linux-gnu\.tar\.gz"   ;;
-            Linux\ *64)      file_pattern="ripgrep-[^-]+-x86_64-unknown-linux-musl\.tar\.gz"   ;;
-            CYGWIN*\ *64)    file_pattern="ripgrep-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            MINGW*\ *64)     file_pattern="ripgrep-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            MSYS*\ *64)      file_pattern="ripgrep-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            Windows*\ *64)   file_pattern="ripgrep-[^-]+-x86_64-pc-windows-msvc\.zip"     ;;
-            *)               file_pattern= binary_error=1 ;;
-        esac
-
-        __download "$(__latest_url "BurntSushi/ripgrep" "$file_pattern")" \
-            --wildcards '*/rg' --strip-components=1
-
-        if [ -f rg ]; then
-            chmod +x rg
-            installed=1
-        else
-            echo "Failed to download ripgrep" >&2
-            failed=1
-        fi
-    fi
-}
-
-install_brew() {
-    echo "Installing homebrew..."
-
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # Add the newly created bin directory to path
-    if [[ -d /opt/homebrew/bin ]]; then
-        path=(/opt/homebrew/bin $path)
+    if [ -d /opt/homebrew/bin ]; then
+        export PATH="/opt/homebrew/bin:$PATH"
     fi
 }
 
-# Update apps
-update_fzf() {
-    echo "Upgrading fzf..."
-    if command -v brew >/dev/null; then
-        brew upgrade fzf
+# Install utilities
+__install_utilities_macos() {
+    __ensure_homebrew_installed
+
+    # Install apps
+    brew install fzf fd zoxide ripgrep bat neovim tmux volta pyenv \
+        pyenv-virtualenv eza gnupg xz switchaudio-osx
+
+    # Setup volta
+    export VOLTA_HOME=$HOME/.volta
+    export PATH="$VOLTA_HOME/bin:$PATH"
+
+    volta install node pnpm
+
+    # Setup pyenv
+    export PYENV_ROOT=$HOME/.pyenv
+    export PATH="$PYENV_ROOT/bin:$PATH"
+
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+
+    pyenv install -s 3
+    pyenv global 3
+    pip install -U pip neovim-remote
+    pyenv virtualenv nvim
+    pyenv activate nvim
+    pip install -U pip neovim
+    pyenv deactivate
+}
+
+__install_utilities() {
+    if ! ((install_utilities)); then
+        return
+    fi
+
+    echo "Installing utilities..."
+    if ((is_macos)); then
+        __install_utilities_macos
     else
-        install_fzf
+        echo "Unsupported OS. Skipping..."
     fi
 }
 
-update_fd() {
-    echo "Upgrading fd..."
-    if command -v brew >/dev/null; then
-        brew upgrade fd
+__install_desktop_apps_macos() {
+    __ensure_homebrew_installed
+
+    brew install --cask android-platform-tools battery chatgpt discord docker \
+        firefox google-chrome iina jordanbaird-ice karabiner-elements \
+        keepingyouawake keka launchbar nightfall rectangle spotify topnotch \
+        transmission visual-studio-code wezterm windscribe \
+        font-jetbrains-mono-nerd-font monitorcontrol prismlauncher
+
+    # Run windscribe installer if it's not installed
+    if ! [ -d /Users/$(whoami)/Library/Application\ Support/Windscribe/ ]; then
+        open /opt/homebrew/Caskroom/windscribe/*/WindscribeInstaller.app
+    fi
+}
+
+__install_desktop_apps() {
+    if ! ((install_desktop_apps)); then
+        return
+    fi
+
+    echo "Installing desktop apps..."
+    if ((is_macos)); then
+        __install_desktop_apps_macos
     else
-        install_fd
+        echo "Unsupported OS. Skipping..."
     fi
 }
 
-update_zoxide() {
-    echo "Upgrading zoxide..."
-    if command -v brew >/dev/null; then
-        brew upgrade zoxide
-    else
-        install_zoxide
-    fi
-}
-
-update_bat() {
-    echo "Upgrading bat..."
-    if command -v brew >/dev/null; then
-        brew upgrade bat
-    else
-        install_bat
-    fi
-}
-
-update_rg() {
-    echo "Upgrading ripgrep..."
-    if command -v brew >/dev/null; then
-        brew upgrade ripgrep
-    else
-        install_rg
-    fi
-}
-
-update_brew() {
-    brew update
-}
-
-bin_path="$HOME/.local/bin"
-mkdir -p "$bin_path"
-
-pushd "$bin_path" &>/dev/null
-
-# Install apps
-for app in "${app_install_list[@]}"; do
-    case $app in
-        fzf) install_fzf;;
-        fd) install_fd;;
-        zoxide) install_zoxide;;
-        bat) install_bat;;
-        rg) install_rg;;
-        brew) install_brew;;
-    esac
-done
-
-# Update apps
-for app in "${app_update_list[@]}"; do
-    case $app in
-        fzf) update_fzf;;
-        fd) update_fd;;
-        zoxide) update_zoxide;;
-        bat) update_bat;;
-        rg) update_rg;;
-        brew) update_brew;;
-    esac
-done
-
-popd &>/dev/null
-
-if ((installed)); then
+__restart_zsh() {
     echo "Restarting zsh..."
-    if [[ "$ZSH_EXECUTION_STRING" ]]; then
+    if [ "$ZSH_EXECUTION_STRING" ]; then
         ((failed)) || exec zsh -c "$ZSH_EXECUTION_STRING"
     else
         exec zsh
     fi
-fi
+}
+
+__clone_repo
+__make_links
+__install_utilities
+__install_desktop_apps
+__restart_zsh
