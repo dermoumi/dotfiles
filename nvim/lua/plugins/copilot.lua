653049e9b2478ba85c2597468f650371002a5938
S Dermoumi <hi@sdrm.me>
2025-02-23 14:44:06 +0000

    Whitelist yaml files it copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,6 +8,7 @@ return {
     event = { "InsertEnter", "CmdlineEnter" },
     opts = {
       suggestion = { auto_trigger = true },
+      filetypes = { yaml = true },
     },
     config = function(_, opts)
       require("copilot").setup(opts)
